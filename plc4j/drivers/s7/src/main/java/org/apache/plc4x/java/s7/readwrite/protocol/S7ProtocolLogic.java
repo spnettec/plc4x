d709ed9b22a6c30bd30a0ebe853fb2d2587cd8ae
heyoulin <heyoulin@gmail.com>
2022-02-07 17:28:57 +0800

    fix protocol

--- plc4j/drivers/s7/src/main/java/org/apache/plc4x/java/s7/readwrite/protocol/S7ProtocolLogic.java
+++ plc4j/drivers/s7/src/main/java/org/apache/plc4x/java/s7/readwrite/protocol/S7ProtocolLogic.java
@@ -245,7 +245,7 @@ public class S7ProtocolLogic extends Plc4xProtocolBase<TPKTPacket> {
      */
     private CompletableFuture<S7Message> readInternal(S7MessageRequest request) {
         CompletableFuture<S7Message> future = new CompletableFuture<>();
-        int thisTpduId = 1;
+        int thisTpduId = 0;
         if (this.s7DriverContext.getControllerType() != S7ControllerType.S7_200)
         {
             thisTpduId = tpduGenerator.getAndIncrement();
@@ -253,7 +253,7 @@ public class S7ProtocolLogic extends Plc4xProtocolBase<TPKTPacket> {
         final int tpduId = thisTpduId;
         // If we've reached the max value for a 16 bit transaction identifier, reset back to 1
         if(tpduGenerator.get() == 0xFFFF) {
-            tpduGenerator.set(1);
+            tpduGenerator.set(0);
         }
 
         // Create a new Request with correct tpuId (is not known before)
