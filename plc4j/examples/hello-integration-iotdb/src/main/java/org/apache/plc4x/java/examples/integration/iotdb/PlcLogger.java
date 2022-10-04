341ddd8b2494aeb5a28663c9c0dd763492d1b590
heyoulin <heyoulin@gmail.com>
2022-10-04 13:04:02 +0800

    merge

--- plc4j/examples/hello-integration-iotdb/src/main/java/org/apache/plc4x/java/examples/integration/iotdb/PlcLogger.java
+++ plc4j/examples/hello-integration-iotdb/src/main/java/org/apache/plc4x/java/examples/integration/iotdb/PlcLogger.java
@@ -18,10 +18,16 @@
  */
 package org.apache.plc4x.java.examples.integration.iotdb;
 
+import org.apache.edgent.function.Supplier;
 import org.apache.edgent.providers.direct.DirectProvider;
+import org.apache.edgent.topology.TStream;
 import org.apache.edgent.topology.Topology;
+import org.apache.plc4x.edgent.PlcConnectionAdapter;
+import org.apache.plc4x.edgent.PlcFunctions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * using this example, you can store data one by one into IoTDB.
@@ -53,6 +59,59 @@ public class PlcLogger {
     static boolean useJDBC;
 
     public static void main(String[] args) throws Exception {
+        CliOptions options = CliOptions.fromArgs(args);
+        if (options == null) {
+            CliOptions.printHelp();
+            // Could not parse.
+            System.exit(1);
+        }
+        useJDBC = options.isUseJDBC();
+        deviceId = String.format("root.%s.%s", options.getStorageGroup(), options.getDevice());
+        sensor = options.getFieldAddress().replace("/", "_").replace(":", "_");
+        timeSeries = String.format("%s.%s", deviceId, sensor);
+        dataType = options.getDevice();
+
+        // Get IoTDB connection
+        if (useJDBC) {
+            ioTDBWriter = new IoTDBWriterWithJDBC(options.getIotdbIpPort(), options.getUser(), options.getPassword());
+        } else {
+            ioTDBWriter = new IoTDBWriterWithSession(options.getIotdbIpPort(), options.getUser(), options.getPassword());
+        }
+
+        //as we do not know whether the storage group is created or not, we can init the storage
+        // group in force.
+        //from v0.9.0 on, IoTDB can automatically create storage group if you enable the parameter
+        //`enable_auto_create_schema` and set the suitable `default_storage_group_level`.
+        // If so, this method can be ignored.
+        ioTDBWriter.initStorageGroup("root." + options.getStorageGroup());
+
+        //register the time series. from v0.9.0 on, IoTDB can automatically create timeseries
+        //if you enable the parameter `enable_auto_create_schema`.
+        //But if you want to explicitly define the series data type, you have to call the method.
+
+        //ioTDBWriter.createTimeseries(timeSeries, dataType);
+
+        // Get a plc connection.
+        try (PlcConnectionAdapter plcAdapter = new PlcConnectionAdapter(options.getConnectionString())) {
+            // Initialize the Edgent core.
+            DirectProvider dp = new DirectProvider();
+            Topology top = dp.newTopology();
+
+            // Define the event stream.
+            // 1) PLC4X source generating a stream of bytes.
+            Supplier<Integer> plcSupplier = PlcFunctions.integerSupplier(plcAdapter,
+                options.getFieldAddress());
+            // 2) Use polling to get an item from the byte-stream in regular intervals.
+            TStream<Integer> source = top.poll(plcSupplier, options.getPollingInterval(),
+                TimeUnit.MILLISECONDS);
+            // 3) Output the events in the stream to IoTDB.
+            source.peek(value -> ioTDBWriter.writeData(deviceId, sensor, System.currentTimeMillis(), value));
+            // Submit the topology and hereby start the event streams.
+            dp.submit(top);
+        }
+        //close IoTDB client.
+        Runtime.getRuntime().addShutdownHook(new Thread(ioTDBWriter::close));
     }
+
 
 }
