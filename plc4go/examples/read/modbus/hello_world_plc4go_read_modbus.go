18560a3321461c185e99de3d2f7bb5d7b93cbf69
heyoulin <heyoulin@gmail.com>
2023-02-20 18:50:53 +0800

    fix char and time can't be written in s7-1500

---
+++ plc4go/examples/read/modbus/hello_world_plc4go_read_modbus.go
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/apache/plc4x/plc4go/pkg/api"
+	"github.com/apache/plc4x/plc4go/pkg/api/drivers"
+	"github.com/apache/plc4x/plc4go/pkg/api/model"
+)
+
+func main() {
+	driverManager := plc4go.NewPlcDriverManager()
+	drivers.RegisterModbusTcpDriver(driverManager)
+
+	// Get a connection to a remote PLC
+	crc := driverManager.GetConnection("modbus-tcp://localhost")
+
+	// Wait for the driver to connect (or not)
+	connectionResult := <-crc
+	if connectionResult.GetErr() != nil {
+		fmt.Printf("error connecting to PLC: %s", connectionResult.GetErr().Error())
+		return
+	}
+	connection := connectionResult.GetConnection()
+
+	// Make sure the connection is closed at the end
+	defer connection.BlockingClose()
+
+	// Prepare a read-request
+	readRequest, err := connection.ReadRequestBuilder().
+		AddTagAddress("field", "holding-register:26:REAL").
+		AddTagAddress("field_bool_single", "holding-register:1:BOOL[1]").
+		AddTagAddress("field_bool_list", "holding-register:10:BOOL[20]").
+		Build()
+	if err != nil {
+		fmt.Printf("error preparing read-request: %s", connectionResult.GetErr().Error())
+		return
+	}
+
+	// Execute a read-request
+	rrc := readRequest.Execute()
+
+	// Wait for the response to finish
+	rrr := <-rrc
+	if rrr.GetErr() != nil {
+		fmt.Printf("error executing read-request: %s", rrr.GetErr().Error())
+		return
+	}
+
+	// Do something with the response
+	if rrr.GetResponse().GetResponseCode("field") != model.PlcResponseCode_OK {
+		fmt.Printf("error an non-ok return code: %s", rrr.GetResponse().GetResponseCode("tag").GetName())
+		return
+	}
+	if rrr.GetResponse().GetResponseCode("field_bool_single") != model.PlcResponseCode_OK {
+		fmt.Printf("error an non-ok return code: %s", rrr.GetResponse().GetResponseCode("tag").GetName())
+		return
+	}
+	if rrr.GetResponse().GetResponseCode("field_bool_list") != model.PlcResponseCode_OK {
+		fmt.Printf("error an non-ok return code: %s", rrr.GetResponse().GetResponseCode("tag").GetName())
+		return
+	}
+
+	value := rrr.GetResponse().GetValue("field")
+	fmt.Printf("Got result %f", value.GetFloat32())
+}
