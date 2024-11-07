9725e3ab3b26e1eb2ebaf52ddce60921da4f7689
heyoulin <heyoulin@gmail.com>
2024-11-07 16:59:16 +0800

    update

--- plc4j/drivers/knxnetip/src/main/generated/org/apache/plc4x/java/knxnetip/readwrite/KnxManufacturer.java
+++ plc4j/drivers/knxnetip/src/main/generated/org/apache/plc4x/java/knxnetip/readwrite/KnxManufacturer.java
@@ -805,9 +805,11 @@ public enum KnxManufacturer {
   M_EBELONG((int) 691, (int) 749, (String) "ebelong"),
   M_ASTRA_LED_AG((int) 692, (int) 750, (String) "Astra-LED AG"),
   M_WONDERFUL_HI_TECH_CO___LTD_((int) 693, (int) 751, (String) "Wonderful Hi-Tech Co., Ltd."),
-  M_ABB___RESERVED((int) 694, (int) 43954, (String) "ABB - reserved"),
+  M_REXLITE_AI_INTEGRATIONS_CO__LTD(
+      (int) 694, (int) 752, (String) "REXLiTE Ai Integrations CO.,LTD"),
+  M_ABB___RESERVED((int) 695, (int) 43954, (String) "ABB - reserved"),
   M_BUSCH_JAEGER_ELEKTRO___RESERVED(
-      (int) 695, (int) 43959, (String) "Busch-Jaeger Elektro - reserved");
+      (int) 696, (int) 43959, (String) "Busch-Jaeger Elektro - reserved");
   private static final Map<Integer, KnxManufacturer> map;
 
   static {
